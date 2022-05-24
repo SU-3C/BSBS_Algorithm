2f586cd515acf1669a05accab74ed589e1964604
Connotation <kkarrkid7@gmail.com>
2022-05-24 23:07:40 +0900

    card solution add

--- Week3/Minseok/p2621.py
+++ Week3/Minseok/p2621.py
@@ -1,56 +1,68 @@
-# 2621 [s4] 카드게임 - 푸는 중
+# 2621 [s4] 카드게임
 
 arr = [input().split() for x in range(0, 5)]
+
+find_table = [
+  ((4, 1)),   # 포카드
+  ((3, 2)),   # 풀하우스
+  ((3,1,1)),  # 트리플
+  ((2,2,1)),  # 투페어
+  ((2,1,1,1)),# 원페어
+  ((1,1,1,1,1)) # 탑 or 스트레이트
+]
+
 color = []
-number = []
+number = {}
+
+def _(i):
+    return [k for k in number if number[k] == i]
 
 result = 0
 
 for x, y in arr:
     color.append(x)
-    number.append(int(y))
+    num = int(y)
+    number.update({num: int(number.get(num) or 0) + 1})
 
-color.sort()
-number.sort()
+num_set = list(set(number.keys()))
+num_array = tuple(sorted(list(number.values()), reverse=True))
+flush = (True if len(set(color)) == 1 else False)
 
-sColor = set(color)
-sNumber = set(number)
 
-print(color, number)
-print("%", len(sColor), len(sNumber))
+# [k for k in number if number[k] == 4] value가 4인 모든 key를 가져옴
 
-if len(sColor) == 1:
-    print("flush")
-    if result < 600:
-        result = 600 + number[4] # Flush
+if find_table[0] == num_array:      # (4, 1) 포카드
+#    result = 800 + [k for k in number if number[k] == 4][-1]
+    result = 800 + _(4)[-1]
 
-if len(sNumber) == 5:
-    if number[0] == number[4] - 4:
-        print("straight")   
-        result = 500 + number[4]    # Straight
-        if sColor == 0:
-            print('straight flush')
-            result = 900 + number[4]    # Straight Flush
-            quit()
-    else:
-        print(f'{number[4]} top')
-        result = 100 + number[4]    # TOP
+elif find_table[1] == num_array:    # (3, 2)    풀하우스
+    result = 700 + [k for k in number if number[k] == 3][-1] * 10 + [k for k in number if number[k] == 2][-1]
 
-if len(sNumber) == 2:
-    if number[1] == number[2]:
-        print("full house")
-    else:
-        print(f"{number[2]} four card")
+elif find_table[2] == num_array:    # (3, 1, 1) 트리플
+    result = 400 + [k for k in number if number[k] == 3][-1]
 
-if len(sNumber) == 3:   # two pair / triple
-    print(f"{number[2]} triple card")
+elif find_table[3] == num_array:    # (2, 2, 1) 투페어
+    pair = [k for k in number if number[k] == 2]
+    pair.sort()
+    result = 300 + pair[1] * 10 + pair[0]
 
-if len(sNumber) == 4:   # One Pair
-    t = a
-    for i in set(t):
-        t.remove(i)
-    print(f"{t[0]} One Pair")
+elif find_table[4] == num_array:    # (2, 1, 1, 1) 원페어
+    result = 200 + [k for k in number if number[k] == 2][-1]
+
+elif find_table[5] == num_array:    # (1, 1, 1, 1, 1) 스트레이트 or TOP
+    temp_list = num_set
+    if temp_list[0] + 4 == temp_list[4]:    # 첫번째와 마지막 차이가 4면 스트레이트
+        result = 500 + temp_list[4]
+    else:                                   # 아닐 경우 TOP
+        result = 100 + max(num_set)
+
+if flush:   # Color가 하나일 경우
+    if result // 100 == 5:  # 스트레이트였다면 스트레이트 플러쉬
+        result = result + 400
+    else:       # 포카드나 풀 하우스일 경우 플러쉬보다 점수가 높음
+        if result < 600:    # result가 600보다 적을 경우에만
+            result = 600 + max(num_set)
     
-
+# print(flush)
 print(result)
 
