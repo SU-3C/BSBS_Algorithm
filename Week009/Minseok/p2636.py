2f3630d0f28e3f52b7ac72b3f192803a8efe93b1
kkarrkid7 <kkarrkid7@gmail.com>
2022-07-23 23:54:13 +0900

    [sovled] 7/23

--- Week009/Minseok/p2636.py
+++ Week009/Minseok/p2636.py
@@ -5,6 +5,57 @@ input = sys.stdin.readline
 
 a, b = map(int, input().split())
 
-cheese = [sys.stdin.readline().rstrip() for x in range(a)]
+cheese = []
+
+for _ in range(a):
+    cheese.append(list(map(int, input().split())))
+
 tu = [(0, -1), (-1, 0), (1, 0), (0, 1)]
 
+
+def search(a, b):
+    if cheese[b][a] == 9 or cheese[b][a] == 2:
+        return False
+    elif cheese[b][a] == 1:
+        return True
+    cheese[b][a] = 9
+    for q, w in tu:
+        try:
+            if search(a+q, b+w):
+                cheese[b+w][a+q] = 2
+        except:
+            pass
+
+def count():
+    i = 0
+    for x in range(a):
+        i += cheese[x].count(1)
+        for y in range(b):
+            if cheese[x][y] == 2 or cheese[x][y] == 9:
+                cheese[x][y] = 0
+    return i
+
+cnt = 0
+
+while (c:=count()) != 0:
+    last = c
+    cnt += 1
+    # print(f'last: {last}, cnt: {cnt}')
+    search(0, 0)
+
+    for x in cheese:
+        # print(x)
+        for y in x:
+            if y == 1:
+                print("☆",end="")
+            elif y == 2:
+                print("★",end="")
+            elif y == 9:
+                print("□",end="")
+            elif y == 0:
+                print("■",end="")
+        print()
+    print()
+
+print(cnt)
+print(last)
