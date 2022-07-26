1a8e84d91b4f79ff7dbdad0ee4934254caff9b1c
yunhwan <79188190+yhyoon1004@users.noreply.github.com>
2022-07-26 13:42:47 +0900

    Update Main_1260.java

--- Week010/Yunhwan/Main_1260.java
+++ Week010/Yunhwan/Main_1260.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class Main_1260 {
 	static StringBuilder result = new StringBuilder();
 	static boolean[] nodesHistory;
-
+	static LinkedList<Node> bfsMainQueue = new LinkedList<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] NodeLinkTarget = br.readLine().split(" ");//첫줄 조건을 입력받는 스트링배열
@@ -23,12 +23,13 @@ public class Main_1260 {
 		for (int i = 0; i < NodeCount; i++) nodesHistory[i] = false;//노드 방문 기록 초기화
 		Node[] Nodes = new Node[NodeCount];
 
-		//노드 생성---------------------------------
+		//노드 생성
 		for (int i = 0; i < NodeCount; i++) {
 			Nodes[i] = new Node(i + 1);
 		}//end of for i
 
-		String[] request;//노드 연결-----------------------
+		//노드 연결
+		String[] request;
 		int A, B;
 		for (int i = 0; i < LinkCount; i++) {
 			request = br.readLine().split(" ");//노드연결 문장 공백기준으로 잘라서 초기화
@@ -38,8 +39,9 @@ public class Main_1260 {
 			Nodes[B].setLinkNode(Nodes[A]);
 			request = null;//노드 연결문장 비워줌
 		}//end for i
+		//노드 구성 완료
 
-		//노드 구성 완료 --------------------------------------------------
+
 		dfs(Nodes[Target - 1]);//Nodes[]인덱스가 0부터시작하므로 -1
 		System.out.println(result);
 		for (int i = 0; i < NodeCount; i++) nodesHistory[i] = false;//노드 방문 기록 초기화
@@ -60,23 +62,23 @@ public class Main_1260 {
 	}//end of method dfs
 
 	public static void bfs(Node targetNode) {
-		if(!nodesHistory[targetNode.value-1]){//타겟 노드의 i요소의 방문기록이 false면 출력 처리
-			result.append(targetNode.value + " ");
-			nodesHistory[targetNode.value - 1] = true;
+		bfsMainQueue.add(targetNode);
+		result.append(targetNode.value+ " ");//인자로 들어온 노드를 기록
+		nodesHistory[targetNode.value - 1]=true;
+		Node target;
+		while(!bfsMainQueue.isEmpty()){
+			target = bfsMainQueue.poll();
+			for (Node i : target.nodes){
+				if(!nodesHistory[i.value-1]){//노드의 i요소의 방문기록이 false면
+					result.append(i.value+ " ");//해당 노드를 기록
+					nodesHistory[i.value - 1]=true;
+					bfsMainQueue.add(i);//
+				}
+			}//end of for Node i : targetNode.nodes
 		}
-		LinkedList<Node> queue = new LinkedList<>();//너비 탐색을 위한 queue
-		for (Node i : targetNode.nodes) {//방문기록이 있으면 건너뛰고 큐에 담아주는 반복문
-			if (!nodesHistory[i.value - 1]) {//노드의 i요소의 방문기록이 false면 //history 인덱스 요소는 node.value 보다 하나 작으므로 -1
-				result.append(i.value + " ");
-				nodesHistory[i.value - 1] = true;
-				queue.add(i);
-			}
-		}//end of for Node i:targetNode.nodes
-		for (Node i : queue){//담아준 큐의 요소를 조회하여 bfs()메서드 반복
-			bfs(i);
-		}//end of for Node i:queue
 
-	}
+
+	}//end of method bfs()
 	static class Node {
 		int value;
 		ArrayList<Node> nodes = new ArrayList<>();
