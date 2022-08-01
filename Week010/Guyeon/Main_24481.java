b7a22091c8bf58ca46d19381f307cec026979c7d
jgy2808 <jgy2808@naver.com>
2022-08-01 12:19:57 +0900

    Upload week11 GY

---
+++ Week010/Guyeon/Main_24481.java
@@ -0,0 +1,54 @@
+package week11;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_24481 {
+    static boolean[] visited;
+    static List<Integer>[] ll;
+    static int[] depthArr;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] in = br.readLine().split(" ");
+        int N = Integer.parseInt(in[0]);
+        int M = Integer.parseInt(in[1]);
+        int R = Integer.parseInt(in[2]);
+        depthArr = new int[N + 1];
+        ll = new List[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            ll[i] = new ArrayList<>();
+            depthArr[i] = -1;
+        }
+        for (int i = 0; i < M; i++) {
+            in = br.readLine().split(" ");
+            int in1 = Integer.parseInt(in[0]);
+            int in2 = Integer.parseInt(in[1]);
+            ll[in1].add(in2);
+            ll[in2].add(in1);
+        }
+        for (int i = 1; i < N + 1; i++) {
+            ll[i].sort(Comparator.naturalOrder());
+        }
+        depthArr[R] = 0;
+        dfs(R);
+        for (int i = 1; i < N + 1; i++) {
+            bw.write(depthArr[i] + "\n");
+        }
+        bw.flush();
+    }
+
+    static void dfs(int start){
+        visited[start] = true;
+        for (int i : ll[start]) {
+            if (depthArr[i] == -1) {
+                depthArr[i] = depthArr[start] + 1;
+            }
+            if (!visited[i])
+                dfs(i);
+
+        }
+    }
+}
