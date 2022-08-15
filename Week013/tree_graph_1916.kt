09bd45c60485294766aba7ca495a22e9fc16f7d6
Grusie <75468060+Grusie@users.noreply.github.com>
2022-08-15 12:50:17 +0900

    Add files via upload

---
+++ Week013/tree_graph_1916.kt
@@ -0,0 +1,15 @@
+package grusie.tree_graph
+
+fun main() {
+    val nodes = readLine()!!.toInt()
+    val bus = readLine()!!.toInt()
+
+
+    for(i in 0 until bus){
+        val (start, end, cost) = readLine()!!.split(" ").map { it.toInt() }
+    }
+
+    fun dfs(){
+
+    }
+}
