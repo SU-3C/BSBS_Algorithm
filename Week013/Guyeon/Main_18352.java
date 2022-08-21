dd325ec2d0490a01f9d3bceeae54121319fe188f
jgy2808 <jgy2808@naver.com>
2022-08-21 18:28:28 +0900

    Update week13 GY

--- Week013/Guyeon/Main_18352.java
+++ Week013/Guyeon/Main_18352.java
@@ -12,57 +12,46 @@ public class Main_18352{
         String[] in = br.readLine().split(" ");
         int N = parseInt(in[0]), M = parseInt(in[1]), K = parseInt(in[2]), X = parseInt(in[3]);
 
-        List<Integer>[] g = new ArrayList[N + 1];
-        int[] res = new int[N + 1];
+        List<Integer>[] road = new ArrayList[N + 1];
+        boolean[] res = new boolean[N + 1];
+        boolean flag = false;
+//        List<Integer> res = new ArrayList<>();
         boolean[] visited = new boolean[N + 1];
         Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
 
-        for (int i = 1; i < N+1; i++) {
-            g[i] = new ArrayList<Integer>();
+        for (int i = 0; i < N+1; i++) {
+            road[i] = new ArrayList<Integer>();
         }
         for (int i = 0; i < M; i++) {
             in = br.readLine().split(" ");
-            int a = parseInt(in[0]);
-            int b = parseInt(in[1]);
-            g[a].add(b);
+            int a = parseInt(in[0]), b = parseInt(in[1]);
+            road[a].add(b);
         }
-//        for (int j = 1; j < N+1; j++) {
-//            bw.write(j + " : ");
-//            for (int i : g[j]) {
-//                bw.write(i + " ");
-//            }
-//            bw.write("\n");
-//        }
-//        bw.write("---------------------\n");
-        int cnt = 0, depth = 0, cur;
+        int depth = 0, cur=1;
 
         q.add(X);
         while (!q.isEmpty()) {
-            cur = q.poll();
-            if (visited[cur]) continue;
-            visited[cur] = true;
-            res[cur] = depth;
-            if (res[cur] == K) cnt++;
-            for(int j : g[cur]){
-                q2.add(j);
-//                bw.write(j + " ");
-            } /*bw.write("\n");*/
-            if (q.isEmpty()){
-                q = q2;
-                q2 = new LinkedList<>();
-                depth++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                cur = q.poll();
+                if(visited[cur]) continue;
+                visited[cur] = true;
+                if (depth == K) {
+                    res[cur] = true;
+                    flag = true;
+                    continue;
+                }
+                for(int j : road[cur]){
+                    q.add(j);
+                }
             }
+            depth++;
         }
-//        bw.write("---------------------\n");
-//        for (int i = 1; i < N + 1; i++) {
-//            bw.write(i + " : " + res[i] + "\n");
-//        }
-//        bw.write("---------------------\n");
-        if (cnt == 0) bw.write("-1");
+        if(!flag) bw.write("-1");
         else {
-            for (int i = 1; i < N + 1; i++) {
-                if(res[i] == K)
+//            res.sort(Integer::compareTo);
+            for (int i=0;i<res.length;i++) {
+                if (res[i])
                     bw.write(i+"\n");
             }
         }
