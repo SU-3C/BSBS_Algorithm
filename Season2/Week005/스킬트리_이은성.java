73c614a4d32e8ba271743a0ae67adec4a7cdc5dd
LEE EUN SUNG <90700649+backgom1@users.noreply.github.com>
2023-07-23 18:38:57 +0900

    Update 스킬트리_이은성 해결

--- Season2/Week005/스킬트리_이은성.java
+++ Season2/Week005/스킬트리_이은성.java
@@ -1,33 +1,30 @@
     public int solution(String skill, String[] skill_trees) {
         List<String> skillOrder = new ArrayList<>();
-        List<Boolean> skillVisit = new ArrayList<>();
         int index = 0;
         int answer = 0;
         for (int i = 0; i < skill.length(); i++) {
             skillOrder.add(String.valueOf(skill.charAt(i)));
-            skillVisit.add(false);
         }
 
         for (String userSkillSet : skill_trees) {
-            index = 0;
-            Collections.fill(skillVisit, false);
+            index =0;
+            boolean skillVisit = true;
             for (int i = 0; i < userSkillSet.length(); i++) {
                 String ASkill = String.valueOf(userSkillSet.charAt(i));
                 if (!skillOrder.contains(ASkill)) {
                     continue;
                 } else {
                     if (!skillOrder.get(index).equals(String.valueOf(userSkillSet.charAt(i)))) {
-                        skillVisit.clear();
+                        skillVisit = false;
                         break;
                     } else {
-                        skillVisit.set(index, true);
                         index++;
                     }
                 }
             }
-            if (skillVisit.contains(true)) {
-                answer++;
-            }
+           if(skillVisit){
+               answer++;
+           }
         }
         return answer;
     }
